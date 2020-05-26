511fe1bc2c92b2cd71dff318d283d6c5158a3ab2
CJ Hillbrand <cjhillbrand@gmail.com>
2020-05-26 13:12:23 -0700

    Main Program with flag handling

---
+++ LineDetection.cpp
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <cstring>
+
+int main(int argc, char* argv[]) {
+    // Entry point for the Line Detection algorithm 
+    // First we parse through the flags that are given
+    // -c -> Designates whether or not we run the canny edge detection
+    //	    on the cpu or GPU 
+    // -h -> Designate whether or not we run the Hough line transform 
+    //	    on the CPU or GPU
+    // -f -> if this flag is set then we read in the file
+    bool cannyCPU = true;
+    bool houghCPU = true;
+    char file[20] = "DEFAULT FILE NAME";
+    int verbose = 1;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "c:h:v:f:")) != -1) {
+	switch(opt) {
+	    case 'c':
+		cannyCPU = strcmp("cpu", optarg) == 0;
+		break;
+	    case 'h':
+		houghCPU = strcmp("cpu", optarg) == 0;
+		break;
+	    case 'f':
+		strcpy(file, optarg);
+		break;
+	    case 'v':
+		verbose = atoi(optarg);
+		break;
+	    default:
+		fprintf(stderr, "Usage:\n-c (cpu|gpu)\n-h (cpu|gpu)\n-v <filename>\n");
+		exit(EXIT_FAILURE);
+	}
+    }
+
+
+}
