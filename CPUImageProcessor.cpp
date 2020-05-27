e856b79189f3204bc17ea91386bcc1ef70010985
CJ Hillbrand <cjhillbrand@gmail.com>
2020-05-27 11:38:15 -0700

    Fixed indentation

--- CPUImageProcessor.cpp
+++ CPUImageProcessor.cpp
@@ -22,19 +22,18 @@ void CPUImageProcessor::preProcess(const Mat& frame, Mat& result) {
 }
 
 void CPUImageProcessor::houghLineTransform(const Mat& frame, Mat& result) {
-	    vector<Vec2f> lines; // will hold the results of the detection
-      HoughLines(frame, lines, 1, CV_PI/180, 150, 0, 0 ); // runs the actual detection
-      // Draw the lines
-    	for( size_t i = 0; i < lines.size(); i++ )
-    	{
-      	float rho = lines[i][0], theta = lines[i][1];
-        Point pt1, pt2;
-        double a = cos(theta), b = sin(theta);
-        double x0 = a*rho, y0 = b*rho;
-        pt1.x = cvRound(x0 + 1000*(-b));
-        pt1.y = cvRound(y0 + 1000*(a));
-        pt2.x = cvRound(x0 - 1000*(-b));
-        pt2.y = cvRound(y0 - 1000*(a));
-        line( result, pt1, pt2, Scalar(0,0,255), 3, LINE_AA);
+	vector<Vec2f> lines; // will hold the results of the detection
+	HoughLines(frame, lines, 1, CV_PI/180, 150, 0, 0 ); // runs the actual detection
+	// Draw the lines
+    	for( size_t i = 0; i < lines.size(); i++ ) {
+	    float rho = lines[i][0], theta = lines[i][1];
+	    Point pt1, pt2;
+	    double a = cos(theta), b = sin(theta);
+	    double x0 = a*rho, y0 = b*rho;
+	    pt1.x = cvRound(x0 + 1000*(-b));
+	    pt1.y = cvRound(y0 + 1000*(a));
+	    pt2.x = cvRound(x0 - 1000*(-b));
+	    pt2.y = cvRound(y0 - 1000*(a));
+	    line( result, pt1, pt2, Scalar(0,0,255), 3, LINE_AA);
     	}
 }
