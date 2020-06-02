cd211f440d9a479cfaf7909fccf6f2ae5a95b57f
RyanWLarson <49253673+RyanWLarson@users.noreply.github.com>
2020-06-01 22:03:22 -0700

    Working GPU kernel function

--- GPUImageProcessor.cu
+++ GPUImageProcessor.cu
@@ -1,38 +1,76 @@
 #include "GPUImageProcessor.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+// Kernel Functions...
+__global__ void colorToGray(const uchar* red, const uchar* blue, const uchar* green,
+    uchar* d_gray, const int rowLength, const int colLength) {
+
+    const int row = blockIdx.y * blockDim.y + threadIdx.y;
+    const int col = blockIdx.x * blockDim.x + threadIdx.x;
+    const int idx = row * rowLength + col;
+
+    float grayval = 0;
+    if (row < colLength && col < rowLength) {
+        grayval = 0.21 * red[idx] + 0.07 * blue[idx] + 0.72 * green[idx];
+        d_gray[idx] = static_cast<uchar>(grayval);
+        //d_gray[idx] = 0;
+    }
+}
+
 GPUImageProcessor::GPUImageProcessor() {
     printf("Not implemented yet");
 }
 
 void GPUImageProcessor::preProcess(const Mat& frame, Mat& result) {
-    printf("Not implemented yet");
+    const int rows = frame.rows, cols = frame.cols;
+    Mat red(rows, cols, CV_8UC1), green(rows, cols, CV_8UC1), blue(rows, cols, CV_8UC1), gray(rows, cols, CV_8UC1);
+    uchar* d_red, * d_green, * d_blue, *d_gray;
+
+    int size = red.step * red.rows;
+    const int THREAD_DIM = 32;// TODO
+    int NUM_OF_BLOCKS = frame.rows / 32; // TODO
+
+    dim3 blocks(ceil((double)frame.cols/32.0), ceil((double)frame.rows/32.0));
+    dim3 threads(32, 32);
+
+    cudaMalloc<unsigned char>(&d_red, size);
+    cudaMalloc<unsigned char>(&d_green, size);
+    cudaMalloc<unsigned char>(&d_blue, size);
+    cudaMalloc<unsigned char>(&d_gray, size);
+
+    threeChannelToArray(frame, red, green, blue);
+
+    cudaMemcpy(d_red, red.ptr(), size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_green, green.ptr(), size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_blue, blue.ptr(), size, cudaMemcpyHostToDevice);
+ 
+    colorToGray<<<blocks, threads >>>(d_red, d_blue, d_green, d_gray, red.cols, red.rows);
+    cudaDeviceSynchronize();
+    cudaMemcpy(gray.ptr(), d_gray, size, cudaMemcpyDeviceToHost);
+
+    imshow("Gray image", gray);
+    waitKey(0);
+
 }
 
 void GPUImageProcessor::houghLineTransform(Mat& frame, Mat& result) {
     printf("Not Implemented yet");
 }
 
-void GPUImageProcessor::threeChannelToArray(const Mat& frame, float* red, float* blue, float* green) {
-    uint8_t* matPtr = (uint8_t*) frame.data;
-    int cn = frame.channels();
+void GPUImageProcessor::threeChannelToArray(const Mat& frame, Mat& red, Mat& blue, Mat& green) {
+    Mat bgrComponents[3];
+    split(frame, bgrComponents);
+    //red = bgrComponents[0].ptr();
+    //blue = bgrComponents[1].ptr();
+    //green = bgrComponents[2].ptr();
 
-    const int rows = frame.rows;
-    const int cols = frame.cols;
-
-    // Iterate over the mat
-    for (int i = 0; i < rows; i++) {
-	for (int j = 0; j < cols; j++) {
-	    blue[i * rows + j] = matPtr[i * cols * cn + j * cn + 0]; // B
-	    green[i * rows + j] = matPtr[i * cols * cn + j * cn + 1]; // G
-	    red[i * rows + j] = matPtr[i * cols * cn + j * cn + 2]; // R
-	}
+    for (int i = 0; i < frame.rows; i++) {
+        for (int j = 0; j < frame.cols; j++) {
+            red.at<uchar>(i, j) = bgrComponents[0].at<uchar>(i,j);
+            blue.at<uchar>(i, j) = bgrComponents[1].at<uchar>(i, j);
+            green.at<uchar>(i, j) = bgrComponents[2].at<uchar>(i, j);
+        }
     }
 }
 
-// Kernel Functions...
-__global__ void colorToGray(const float* red, const float* blue, const float* green, float* gray, const int n) {
-    const int row = blockIdx.y * blockDim.y + threadIdx.y;
-    const int col = blockIdx.x * blockDim.x + threadIdx.x;
-    const int idx = row * n + col;
-
-    gray[idx] = 0.21 * red[idx] + 0.07 * blue[idx] + 0.72 * green[idx];
-}
