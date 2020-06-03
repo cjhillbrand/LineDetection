c9b3747047ba81dd095cbb964527f2f7ec7f8243
RyanWLarson <49253673+RyanWLarson@users.noreply.github.com>
2020-06-02 21:47:27 -0700

    added signatures for smooth/edge

--- GPUImageProcessor.h
+++ GPUImageProcessor.h
@@ -3,20 +3,23 @@
 #ifndef GPU_IMAGE_PROCESSOR
 #define GPU_IMAGE_PROCESSOR
 class GPUImageProcessor : public ImageProcessor {
-    public:
-	GPUImageProcessor();
-	void preProcess(const Mat&, Mat&) override;
-	void houghLineTransform(Mat&, Mat&) override;
-    private:
-//    	__device__ void colorToGray(const float*, const float*, const float*, float*, const int);
-    //	__kernel__ smoothing
-    //	__kernel__ threshold
-    //	if we get to it __kernel__ canny
-    //
-    //	mattoarray helper methods
-    //	arraytomat helper methods
-    //
-    	void threeChannelToArray(const Mat&, float*, float*, float*);
+public:
+    GPUImageProcessor();
+    void preProcess(const Mat&, Mat&) override;
+    void houghLineTransform(Mat&, Mat&) override;
+private:
+    //    	__device__ void colorToGray(const float*, const float*, const float*, float*, const int);
+        //	__kernel__ smoothing
+        //	__kernel__ threshold
+        //	if we get to it __kernel__ canny
+        //
+        //	mattoarray helper methods
+        //	arraytomat helper methods
+        //
+    void threeChannelToArray(const Mat&, Mat&, Mat&, Mat&);
+    void GPUImageSmooth(const Mat& input, Mat& output);
+    void GPUImageGray(const Mat& input, Mat& output);
+    void GPUImageEdge(const Mat& input, Mat& output);
 };
 
 
