96f984cb3719f627dd2d59f66599cebf1ed41ea1
RyanWLarson <49253673+RyanWLarson@users.noreply.github.com>
2020-06-04 21:08:45 -0700

    final commit

--- GPUImageProcessor.cu
+++ GPUImageProcessor.cu
@@ -2,89 +2,82 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
+const int SIZE = 1024; // Number of threads per block (32 x 32)
+
 // Kernel Functions...
 __global__ void colorToGray(const uchar* input, uchar* d_gray, const int rowLength, const int colLength, const int colorFlag) {
-
     const int row = blockIdx.y * blockDim.y + threadIdx.y;
     const int col = blockIdx.x * blockDim.x + threadIdx.x;
     const int idx = row * rowLength + col;
-
+    const int tid = threadIdx.x * blockDim.x + threadIdx.y;
+   
     float grayval = 0;
     if (row < colLength && col < rowLength) {
-        if (colorFlag == 1) {
-            d_gray[idx] += static_cast<uchar>(0.21 * input[idx]);
+        if (colorFlag == 1) {   
+            d_gray[idx] += static_cast<uchar>(0.21 * input[idx]); //  Red
         }
         else if (colorFlag == 2) {
-            d_gray[idx] += static_cast<uchar>(0.07 * input[idx]);
+            d_gray[idx] += static_cast<uchar>(0.07 * input[idx]); //  Blue
         }
         else {
-            d_gray[idx] += static_cast<uchar>(0.72 * input[idx]);
-
+            d_gray[idx] += static_cast<uchar>(0.72 * input[idx]); //  Green
         }
-        //d_gray[idx] = static_cast<uchar>(0.21 * red[idx] + 0.07 * blue[idx] + 0.72 * green[idx]);
     }
 }
 
-//__constant__ const int halfKernel = 1;
+__constant__ int halfKernel = 1;
 
-__global__ void convolution(const uchar* input, uchar* output, const uchar* g_kernel, const int kernelDim,
+__global__ void convolution(const uchar* input, uchar* output, const uchar* kernel, const int kernelDim,
                              const int rows, const int cols, const float reduction, const int sobelFlag = 0, uchar* mask = 0) {
 
     const int row = blockIdx.y * blockDim.y + threadIdx.y;
     const int col = blockIdx.x * blockDim.x + threadIdx.x;
+    const int tid = threadIdx.x * blockDim.x + threadIdx.y;
     const int idx = row * cols + col;
-    const int halfKernel = kernelDim / 2;
+    float sum = 0;
 
-    __shared__ uchar kernel[9];
-
-    if (threadIdx.x < 9) {
-        kernel[threadIdx.x] = g_kernel[threadIdx.x];
-    } 
-    __syncthreads();
-    
     if (row < rows && col < cols) {
         if (row < halfKernel || col < halfKernel || row + halfKernel > rows || col + halfKernel > cols) {
-            // edge case, do nothing for now
+            // global edge case, do nothing for now
             output[idx] = input[idx];
         }
         else {
-            float sum = 0;
-            // i iterates over the columns, j iterates over the rows
+            // i iterates over the columns, j iterates over the rows in inner block
             if (kernelDim == 9 && !sobelFlag) {
-                sum += input[idx - 1 * cols - 1] * kernel[(- 1 + halfKernel) * kernelDim + (- 1 + halfKernel)]; // -1 -1
-                sum += input[idx  * cols - 1] * kernel[(halfKernel) * kernelDim + (-1 + halfKernel)];           //  0 -1
-                sum += input[idx + 1 * cols - 1] * kernel[(1 + halfKernel) * kernelDim + (-1 + halfKernel)];    //  1 -1
-                sum += input[idx -1 * cols] * kernel[(-1 + halfKernel) * kernelDim + (halfKernel)];             // -1  0
-                sum += input[idx * cols] * kernel[(halfKernel) * kernelDim + (halfKernel)];     //  0  0
-                sum += input[idx + 1 * cols] * kernel[(1 + halfKernel) * kernelDim + (halfKernel)];             //  1  0
-                sum += input[idx - 1 * cols + 1] * kernel[(-1 + halfKernel) * kernelDim + (1 + halfKernel)];    // -1  1
-                sum += input[idx* cols + 1] * kernel[(halfKernel) * kernelDim + (1 + halfKernel)];              //  0  1
-                sum += input[idx + 1 * cols + 1] * kernel[(1 + halfKernel) * kernelDim + (1 + halfKernel)];     //  1  1
+                sum += input[idx - 1 * cols - 1] * kernel[(- 1 + halfKernel) * kernelDim + (- 1 + halfKernel)];  // -1 -1
+                sum += input[idx * cols - 1] * kernel[(halfKernel) * kernelDim + (-1 + halfKernel)];             //  0 -1
+                sum += input[idx + 1 * cols - 1] * kernel[(1 + halfKernel) * kernelDim + (-1 + halfKernel)];     //  1 -1
+                sum += input[idx -1 * cols] * kernel[(-1 + halfKernel) * kernelDim + (halfKernel)];              // -1  0
+                sum += input[idx * cols] * kernel[(halfKernel) * kernelDim + (halfKernel)];                      //  0  0
+                sum += input[idx + 1 * cols] * kernel[(1 + halfKernel) * kernelDim + (halfKernel)];              //  1  0
+                sum += input[idx - 1 * cols + 1] * kernel[(-1 + halfKernel) * kernelDim + (1 + halfKernel)];     // -1  1
+                sum += input[idx * cols + 1] * kernel[(halfKernel) * kernelDim + (1 + halfKernel)];              //  0  1
+                sum += input[idx + 1 * cols + 1] * kernel[(1 + halfKernel) * kernelDim + (1 + halfKernel)];      //  1  1
             }
             else if (sobelFlag) {
                 if (sobelFlag == 1) {
-                    sum -= input[idx - 1 * cols - 1] * kernel[(-1 + halfKernel) * kernelDim + (-1 + halfKernel)]; 
+                    sum -= input[idx - 1 * cols - 1] * kernel[(-1 + halfKernel) * kernelDim + (-1 + halfKernel)];
                 } else {
-                    sum += input[idx - 1 * cols - 1] * kernel[(-1 + halfKernel) * kernelDim + (-1 + halfKernel)]; 
+                    sum += input[idx - 1 * cols - 1] * kernel[(-1 + halfKernel) * kernelDim + (-1 + halfKernel)];
                 }
                 if (sobelFlag == 2) {
-                    sum += input[idx + 0 * cols - 1] * kernel[(halfKernel)*kernelDim + (-1 + halfKernel)];          
+                    sum += input[idx + 0 * cols - 1] * kernel[(halfKernel)*kernelDim + (-1 + halfKernel)];
                 }
                 sum += input[idx + 1 * cols - 1] * kernel[(1 + halfKernel) * kernelDim + (-1 + halfKernel)];   
                 if (sobelFlag == 1) {
-                    sum -= input[idx - 1 * cols] * kernel[(-1 + halfKernel) * kernelDim + (halfKernel)]; 
+                    sum -= input[idx - 1 * cols] * kernel[(-1 + halfKernel) * kernelDim + (halfKernel)];
                 }
                 if (sobelFlag == 1) {
                     sum += input[idx + 1 * cols] * kernel[(1 + halfKernel) * kernelDim + (halfKernel)];
                 }
-                sum -= input[idx - 1 * cols + 1] * kernel[(-1 + halfKernel) * kernelDim + (1 + halfKernel)];   
+                sum -= input[idx - 1 * cols + 1] * kernel[(-1 + halfKernel) * kernelDim + (1 + halfKernel)];
                 if (sobelFlag == 2) {
-                    sum -= input[idx + 0 * cols + 1] * kernel[(halfKernel)*kernelDim + (1 + halfKernel)];          
+                    sum -= input[idx + 0 * cols + 1] * kernel[(halfKernel)*kernelDim + (1 + halfKernel)];
                 }
                 if (sobelFlag == 2) {
-                    sum -= input[idx + 1 * cols + 1] * kernel[(1 + halfKernel) * kernelDim + (1 + halfKernel)];   
+                    sum -= input[idx + 1 * cols + 1] * kernel[(1 + halfKernel) * kernelDim + (1 + halfKernel)];
                 } else {
-                    sum += input[idx + 1 * cols + 1] * kernel[(1 + halfKernel) * kernelDim + (1 + halfKernel)];   
+                    sum += input[idx + 1 * cols + 1] * kernel[(1 + halfKernel) * kernelDim + (1 + halfKernel)];
                 }
             }
             // TODO potentially remove this section
@@ -121,37 +114,71 @@ __global__ void threshold(const uchar* input, uchar* output, const int rows, con
     }
 }
 
-__global__ void sobelGradientMagnitude(uchar* output, const uchar* outputGx, const uchar* outputGy, const uchar* maskGx, const uchar* maskGy,
+__global__ void sobelGradientMagnitude(uchar* output, const uchar* outGx, const uchar* outGy, const uchar* maskGx, const uchar* maskGy,
                     const int rows, const int cols) {
     const int row = blockIdx.y * blockDim.y + threadIdx.y;
     const int col = blockIdx.x * blockDim.x + threadIdx.x;
     const int idx = row * cols + col;
+    const int tid = threadIdx.x * blockDim.x + threadIdx.y;
+    
+    __shared__ uchar outputGx[SIZE];
+    __shared__ uchar outputGy[SIZE];
+    
+    outputGx[tid] = outGx[idx];
+    outputGy[tid] = outGy[idx];
 
+    __syncthreads();
+    
     if (row < rows && col < cols) {
+        
         output[idx] = static_cast<uchar>(sqrt(pow(maskGx[idx]
-            == 1 ? -static_cast<float>(outputGx[idx]) : static_cast<float>(outputGx[idx]), 2)
-            + pow(maskGy[idx] == 1 ? -static_cast<float>(outputGy[idx]) : static_cast<float>(outputGy[idx]), 2)));
+            == 1 ? -static_cast<float>(outputGx[tid]) : static_cast<float>(outputGx[tid]), 2)
+            + pow(maskGy[idx] == 1 ? -static_cast<float>(outputGy[tid]) : static_cast<float>(outputGy[tid]), 2)));
     }
 }
 
-GPUImageProcessor::GPUImageProcessor() {
-    printf("Not implemented yet");
+__global__ void TCTOA(const uchar* bgrComponent, uchar* output, const int rows, const int cols) {
+    const int row = blockIdx.y * blockDim.y + threadIdx.y;
+    const int col = blockIdx.x * blockDim.x + threadIdx.x;
+    const int idx = row * cols + col;
+    const int tid = threadIdx.x * blockDim.x + threadIdx.y;
+
+    __shared__ uchar bgr[SIZE];
+    bgr[tid] = bgrComponent[idx];
+
+    __syncthreads();
+
+    output[idx] = bgr[tid];
+}
+
+GPUImageProcessor::GPUImageProcessor(std::string filename) : ImageProcessor(filename ) {}
+
+void GPUImageProcessor::preProcess(const Mat& frame, Mat& output) {  
+   
+    Mat gray(frame.rows, frame.cols, CV_8UC1);
+    Mat smooth(frame.rows, frame.cols, CV_8UC1);
+    Mat thresh(frame.rows, frame.cols, CV_8UC1);
+    
+    GPUImageGray(frame, gray);
+    GPUImageSmooth(gray, smooth);
+    GPUImageThreshold(smooth, thresh);
+    GPUImageEdge(thresh, output);
 }
 
 void GPUImageProcessor::GPUImageThreshold(const Mat& input, Mat& output) {
     const int rows = input.rows, cols = input.cols;
     uchar* d_input, * d_output;
     const int size = rows * cols;
+    const dim3 blocks(ceil((double)cols / 32.0), ceil((double)rows / 32.0));
+    const dim3 threads(32, 32);
 
-    const uchar lowerBound = 220;
+    //220
+    const uchar lowerBound = 230;
 
     cudaMalloc<unsigned char>(&d_input, size);
     cudaMalloc<unsigned char>(&d_output, size);
 
-    cudaMemcpy(d_input, input.ptr(), size, cudaMemcpyHostToDevice);
-
-    const dim3 blocks(ceil((double)cols / 32.0), ceil((double)rows / 32.0));
-    const dim3 threads(32, 32);
+    cudaMemcpyAsync(d_input, input.ptr(), size, cudaMemcpyHostToDevice);
 
     threshold << <blocks, threads >> > (d_input, d_output, rows, cols, lowerBound);
     cudaDeviceSynchronize();
@@ -160,33 +187,6 @@ void GPUImageProcessor::GPUImageThreshold(const Mat& input, Mat& output) {
 
     cudaFree(d_input);
     cudaFree(d_output);
-}
-
-void GPUImageProcessor::preProcess(const Mat& frame, Mat& output) {  
-    Mat gray(frame.rows, frame.cols, CV_8UC1);
-    Mat smooth(frame.rows, frame.cols, CV_8UC1);
-    Mat thresh(frame.rows, frame.cols, CV_8UC1);
-
-    std::cout << std::endl;
-    Start();
-    GPUImageGray(frame, gray);
-    Stop();
-    printTime("GPU Gray");
-    
-    Start();
-    GPUImageSmooth(gray, smooth);
-    Stop();
-    printTime("GPU Smooth");
-
-    Start();
-    GPUImageThreshold(smooth, thresh);
-    Stop();
-    printTime("GPU Thresh");
-
-    Start();
-    GPUImageEdge(thresh, output);
-    Stop();
-    printTime("GPU Edge");
 }
 
 void GPUImageProcessor::GPUImageEdge(const Mat& input, Mat& output) {
@@ -208,19 +208,25 @@ void GPUImageProcessor::GPUImageEdge(const Mat& input, Mat& output) {
     const dim3 blocks(ceil((double)cols / 32.0), ceil((double)rows / 32.0));
     const dim3 threads(32, 32);
 
+    const int NUM_OF_STREAMS = 2;
+    cudaStream_t streams[NUM_OF_STREAMS];
+   
+    cudaStreamCreate(&streams[0]);
+    cudaStreamCreate(&streams[1]);
+
     // Gx
     cudaMalloc<unsigned char>(&d_input, size);
     cudaMalloc<unsigned char>(&d_kernelGx, 9);
     cudaMalloc<unsigned char>(&d_maskGx, size);
     cudaMalloc<unsigned char>(&d_outputGx, size);
-    cudaMemcpy(d_input, input.ptr(), size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_kernelGx, kernelGx.ptr(), 9, cudaMemcpyHostToDevice);
-
-    convolution << <blocks, threads >> > (d_input, d_outputGx, d_kernelGx, 3, rows, cols, reduction, 1, d_maskGx);
-    //cudaDeviceSynchronize();
+    cudaMemcpyAsync(d_input, input.ptr(), size, cudaMemcpyHostToDevice);
+    cudaMemcpyAsync(d_kernelGx, kernelGx.ptr(), 9, cudaMemcpyHostToDevice);
     
-    cudaMemcpy(outputGx.ptr(), d_outputGx, size, cudaMemcpyDeviceToHost);
-    cudaMemcpy(maskGx.ptr(), d_maskGx, size, cudaMemcpyDeviceToHost);
+    convolution << <blocks, threads, 0, streams[0] >> > (d_input, d_outputGx, d_kernelGx, 3, rows, cols, reduction, 1, d_maskGx);
+
+    cudaMemcpyAsync(outputGx.ptr(), d_outputGx, size, cudaMemcpyDeviceToHost);
+    cudaMemcpyAsync(maskGx.ptr(), d_maskGx, size, cudaMemcpyDeviceToHost);
+
     cudaFree(d_kernelGx);
 
     // Gy
@@ -228,33 +234,24 @@ void GPUImageProcessor::GPUImageEdge(const Mat& input, Mat& output) {
     cudaMalloc<unsigned char>(&d_maskGy, size);
     cudaMalloc<unsigned char>(&d_outputGy, size);
 
-    cudaMemcpy(d_kernelGy, kernelGy.ptr(), 9, cudaMemcpyHostToDevice);
+    cudaMemcpyAsync(d_kernelGy, kernelGy.ptr(), 9, cudaMemcpyHostToDevice);
+ 
 
-    convolution <<<blocks, threads >>> (d_input, d_outputGy, d_kernelGy, 3, rows, cols, reduction, 2, d_maskGy);
+    convolution <<<blocks, threads, 0, streams[1] >>> (d_input, d_outputGy, d_kernelGy, 3, rows, cols, reduction, 2, d_maskGy);
     cudaDeviceSynchronize();
 
-    cudaMemcpy(outputGy.ptr(), d_outputGy, size, cudaMemcpyDeviceToHost);
-    cudaMemcpy(maskGy.ptr(), d_maskGy, size, cudaMemcpyDeviceToHost);
+    cudaMemcpyAsync(outputGy.ptr(), d_outputGy, size, cudaMemcpyDeviceToHost);
+    cudaMemcpyAsync(maskGy.ptr(), d_maskGy, size, cudaMemcpyDeviceToHost);
 
     cudaFree(d_kernelGy);
     cudaFree(d_input);
 
-    // sqrt(Gx^2 + Gy^2
     cudaMalloc(&d_output, size);
 
+    // sqrt(Gx^2 + Gy^2
     sobelGradientMagnitude << <blocks, threads >> > (d_output, d_outputGx, d_outputGy, d_maskGx, d_maskGy, rows, cols);
     cudaDeviceSynchronize();
 
-    /*
-    // Process Gx and Gy
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            output.at<uchar>(i, j) = static_cast<uchar>(sqrt(pow(maskGx.at<uchar>(i, j) 
-                == 1 ? -static_cast<float>(outputGx.at<uchar>(i, j)) : static_cast<float>(outputGx.at<uchar>(i, j)), 2) 
-                + pow(maskGy.at<uchar>(i, j) == 1 ? -static_cast<float>(outputGy.at<uchar>(i, j)) : static_cast<float>(outputGy.at<uchar>(i, j)), 2)));
-        }
-    }
-    */
 
     cudaMemcpy(output.ptr(), d_output, size, cudaMemcpyDeviceToHost);
 
@@ -271,28 +268,32 @@ void GPUImageProcessor::GPUImageGray(const Mat& input, Mat& output) {
     uchar* d_red, * d_green, * d_blue, * d_gray;
 
     const int size = red.step * red.rows;
+    const int NUM_OF_STREAMS = 3;
+    cudaStream_t streams[NUM_OF_STREAMS];
 
     dim3 blocks(ceil((double)cols / 32.0), ceil((double)rows / 32.0));
     dim3 threads(32, 32);
 
     threeChannelToArray(input, red, green, blue);
 
-    Start();
     cudaMalloc<unsigned char>(&d_gray, size);
-
     cudaMalloc<unsigned char>(&d_red, size);
-    cudaMemcpy(d_red, red.ptr(), size, cudaMemcpyHostToDevice);
-    colorToGray << <blocks, threads >> > (d_red, d_gray, red.cols, red.rows, 1);
-
     cudaMalloc<unsigned char>(&d_green, size);
-    cudaMemcpy(d_green, green.ptr(), size, cudaMemcpyHostToDevice);
-    colorToGray << <blocks, threads >> > (d_green, d_gray, red.cols, red.rows, 2);
+    cudaMalloc<unsigned char>(&d_blue, size);
 
-    cudaMalloc<unsigned char>(&d_blue, size);    
-    cudaMemcpy(d_blue, blue.ptr(), size, cudaMemcpyHostToDevice);
-    colorToGray << <blocks, threads >> > (d_blue, d_gray, red.cols, red.rows, 3);
+    cudaMemcpyAsync(d_red, red.ptr(), size, cudaMemcpyHostToDevice);
+    cudaMemcpyAsync(d_green, green.ptr(), size, cudaMemcpyHostToDevice);
+    cudaMemcpyAsync(d_blue, blue.ptr(), size, cudaMemcpyHostToDevice);
+
+    cudaStreamCreate(&streams[0]);
+    cudaStreamCreate(&streams[1]);
+    cudaStreamCreate(&streams[2]);
+
+    colorToGray << <blocks, threads, 0, streams[0] >> > (d_red, d_gray, red.cols, red.rows, 1);
+    colorToGray << <blocks, threads, 0, streams[1] >> > (d_green, d_gray, red.cols, red.rows, 2);
+    colorToGray << <blocks, threads, 0, streams[2] >> > (d_blue, d_gray, red.cols, red.rows, 3);
     cudaDeviceSynchronize();
-
+    
     cudaMemcpy(output.ptr(), d_gray, size, cudaMemcpyDeviceToHost);
     cudaFree(d_red);
     cudaFree(d_blue);
@@ -312,8 +313,8 @@ void GPUImageProcessor::GPUImageSmooth(const Mat& input, Mat& output) {
     cudaMalloc<unsigned char>(&d_output, size);
     cudaMalloc<unsigned char>(&d_kernel, 9);
 
-    cudaMemcpy(d_input, input.ptr(), size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_kernel, kernel.ptr(), 9, cudaMemcpyHostToDevice);
+    cudaMemcpyAsync(d_input, input.ptr(), size, cudaMemcpyHostToDevice);
+    cudaMemcpyAsync(d_kernel, kernel.ptr(), 9, cudaMemcpyHostToDevice);
 
     const dim3 blocks(ceil((double)cols / 32.0), ceil((double)rows / 32.0));
     const dim3 threads(32, 32);
@@ -334,14 +335,50 @@ void GPUImageProcessor::houghLineTransform(Mat& frame, Mat& result) {
 
 void GPUImageProcessor::threeChannelToArray(const Mat& frame, Mat& red, Mat& blue, Mat& green) {
     Mat bgrComponents[3];
-    split(frame, bgrComponents);
 
-    for (int i = 0; i < frame.rows; i++) {
-        for (int j = 0; j < frame.cols; j++) {
-            red.at<uchar>(i, j) = bgrComponents[0].at<uchar>(i,j);
-            blue.at<uchar>(i, j) = bgrComponents[1].at<uchar>(i, j);
-            green.at<uchar>(i, j) = bgrComponents[2].at<uchar>(i, j);
-        }
-    }
+    const int size = frame.rows * frame.cols;
+    const int rows = frame.rows, cols = frame.cols;
+
+    const dim3 blocks(ceil((double)cols / 32.0), ceil((double)rows / 32.0));
+    const dim3 threads(32, 32);
+
+    const int NUM_OF_STREAMS = 3;
+    cudaStream_t streams[NUM_OF_STREAMS];
+    cudaStreamCreate(&streams[0]);
+    cudaStreamCreate(&streams[1]);
+    cudaStreamCreate(&streams[2]);
+
+    split(frame, bgrComponents);
+ 
+    uchar *d_redComponent, *d_blueComponent, *d_greenComponent, *d_red, *d_blue, *d_green;
+
+    cudaMalloc<unsigned char>(&d_red, size);
+    cudaMalloc<unsigned char>(&d_blue, size);
+    cudaMalloc<unsigned char>(&d_green, size);
+    cudaMalloc<unsigned char>(&d_redComponent, size);
+    cudaMalloc<unsigned char>(&d_blueComponent, size);
+    cudaMalloc<unsigned char>(&d_greenComponent, size);
+
+   
+    cudaMemcpyAsync(d_redComponent, bgrComponents[0].ptr(), size, cudaMemcpyHostToDevice);
+    cudaMemcpyAsync(d_blueComponent, bgrComponents[1].ptr(), size, cudaMemcpyHostToDevice);
+    cudaMemcpyAsync(d_greenComponent, bgrComponents[2].ptr(), size, cudaMemcpyHostToDevice);
+
+    TCTOA << <blocks, threads, 0, streams[0] >> > (d_redComponent, d_red, rows, cols);
+    TCTOA << <blocks, threads, 0, streams[1] >> > (d_blueComponent, d_blue, rows, cols);
+    TCTOA << <blocks, threads, 0, streams[2] >> > (d_greenComponent, d_green, rows, cols);
+    cudaDeviceSynchronize();
+
+    cudaMemcpyAsync(red.ptr(), d_red, size, cudaMemcpyDeviceToHost);
+    cudaMemcpyAsync(blue.ptr(), d_blue, size, cudaMemcpyDeviceToHost);
+    cudaMemcpyAsync(green.ptr(), d_green, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_red);
+    cudaFree(d_blue);
+    cudaFree(d_green);
+    cudaFree(d_redComponent);
+    cudaFree(d_blueComponent);
+    cudaFree(d_greenComponent);
+    
 }
 
